e9d8300a93d4036784a603d7183b1f6e2cfda205
Aneesh P V <aneeshpvwiz@gmail.com>
2025-02-09 17:33:06 +0530

    fixed cors issue

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
+	github.com/gin-contrib/cors v1.7.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
@@ -43,7 +44,7 @@ require (
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/protobuf v1.36.0 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.11 // indirect
 	gorm.io/gorm v1.25.12 // indirect
--- routes/router.go
+++ routes/router.go
@@ -2,7 +2,10 @@ package routes
 
 import (
 	"apps90-hms/middlewares"
+	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +13,17 @@ func InitRoutes() *gin.Engine {
 	router := gin.Default()
 	//router.Use(middlewares.APIResponseMiddleware())
 	router.Use(middlewares.APIErrorMiddleware())
+
+	// CORS Middleware
+	FrontendUrl := os.Getenv("FRONTEND_URL")
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{FrontendUrl}, // Allow frontend domain
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true, // If using cookies or auth headers
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Register routes
 	AuthRoutes(router)
